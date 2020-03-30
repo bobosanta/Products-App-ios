6f93685da10844de038026fa047096c4ef81d28f
Santamarian Bogdan <santamarian.bogdan@gmail.com>
2020-03-30 18:39:38 +0300

    Add Register screen UI elements and IBOutlets/IBActions

--- Products/Products-Root/Screens/Authentication/Register/RegisterViewController.swift
+++ Products/Products-Root/Screens/Authentication/Register/RegisterViewController.swift
@@ -14,6 +14,21 @@ protocol RegisterViewControllerDelegate: class {
 
 class RegisterViewController: BaseViewController {
     
+    // MARK: IBOutlets
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var addProfileImageButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var birthDateTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var signinButton: UIButton!
+    @IBOutlet weak var showPasswordButton: UIButton!
+    
+    
     // MARK: - Properties
     weak var delegate: RegisterViewControllerDelegate?
     private var viewModel: RegisterViewModel!
@@ -26,8 +41,18 @@ class RegisterViewController: BaseViewController {
     }
     
     // MARK: IBActions
-    @IBAction func buttonInRegisterPressed(_ sender: Any) {
-    
+    @IBAction func registerButtonTapped(_ sender: Any) {
     }
+    
+    @IBAction func showPasswordButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addProfileImageButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func signinButtonTapped(_ sender: Any) {
+    }
+    
+    
     
 }
